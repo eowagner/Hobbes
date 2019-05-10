5334c35470dde611f582aaaec5b19419265d85f9
eowagner <e.o.wagner@gmail.com>
2019-05-09 23:04:09 -0500

    Database config for heroku

--- Hobbes/Hobbes/settings.py
+++ Hobbes/Hobbes/settings.py
@@ -12,8 +12,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import dotenv, dj_database_url
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
@@ -75,6 +82,10 @@ WSGI_APPLICATION = 'Hobbes.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+
 
 DATABASES = {
     #'default': {
