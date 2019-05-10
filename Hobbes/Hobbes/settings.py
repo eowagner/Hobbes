2a2da5ddda7dc1e0f27dc2267e783f3cafdfc889
eowagner <e.o.wagner@gmail.com>
2019-05-09 23:07:23 -0500

    added localhost to allowed hosts

--- Hobbes/Hobbes/settings.py
+++ Hobbes/Hobbes/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'anep!e56nu)z^8qf-$=a)+t!svdy)ooos2q3=q+xo6fkxm&n$p'
 DEBUG = True
 
 #ALLOWED_HOSTS = []
-ALLOWED_HOSTS = ['afternoon-oasis-70075.herokuapp.com']
+ALLOWED_HOSTS = ['afternoon-oasis-70075.herokuapp.com', 'localhost']
 
 
 # Application definition
