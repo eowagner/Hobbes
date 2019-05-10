b820a86dfc9396431d7e7c9f349a0dab7b6ec52d
eowagner <e.o.wagner@gmail.com>
2019-05-09 23:13:23 -0500

    dotenv doesn't appear to work on heroku?

--- Hobbes/Hobbes/settings.py
+++ Hobbes/Hobbes/settings.py
@@ -12,15 +12,15 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-import dotenv, dj_database_url
-
+#import dotenv, dj_database_url
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-dotenv_file = os.path.join(BASE_DIR, ".env")
-if os.path.isfile(dotenv_file):
-    dotenv.load_dotenv(dotenv_file)
+# dotenv_file = os.path.join(BASE_DIR, ".env")
+# if os.path.isfile(dotenv_file):
+#    dotenv.load_dotenv(dotenv_file)
 
 
 # Quick-start development settings - unsuitable for production
